755154488308745a401a4877b22843f4dabaa2e3
mirrorsluck <Ginger@Ginger-Komputer>
2017-04-23 14:47:04 +0200

    1.01

--- src/main/java/pl/sternik/kk/weekend/web/controlers/KlaserController.java
+++ src/main/java/pl/sternik/kk/weekend/web/controlers/KlaserController.java
@@ -17,47 +17,52 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import pl.sternik.kk.weekend.entities.Moneta;
+import pl.sternik.kk.weekend.entities.Status;
+import pl.sternik.kk.weekend.services.KlaserService;
+import pl.sternik.kk.weekend.services.NotificationService;
+
 
 @Controller
 public class KlaserController {
 
-//    @Autowired
-    // @Qualifier("spring")
-//    private KlaserService klaserService;
+    @Autowired
+    //@Qualifier("spring")
+    private KlaserService klaserService;
 
-//    @Autowired
-//    private NotificationService notificationService;
+    @Autowired
+    private NotificationService notificationService;
 
-//    @ModelAttribute("statusyAll")
-//    public List<Status> populateStatusy() {
-//        return Arrays.asList(Status.ALL);
-//    }
+    @ModelAttribute("statusyAll")
+    public List<Status> populateStatusy() {
+        return Arrays.asList(Status.ALL);
+    }
 
-//    @ModelAttribute("coinsAll")
-//    public List<Moneta> populateCoins() {
-//        return this.klaserService.findAll();
-//    }
+    @ModelAttribute("coinsAll")
+    public List<Moneta> populateCoins() {
+        return this.klaserService.findAll();
+    }
 
-//    @ModelAttribute("coinsToSell")
-//    public List<Moneta> populateCoinsToSell() {
-//        return this.klaserService.findAllToSell();
-//    }
+    @ModelAttribute("coinsToSell")
+    public List<Moneta> populateCoinsToSell() {
+        return this.klaserService.findAllToSell();
+    }
 
-//    @ModelAttribute("coinsLast3")
-//    public List<Moneta> populateLast3Coins() {
-//        return this.klaserService.findLatest3();
-//    }
+    @ModelAttribute("coinsLast3")
+    public List<Moneta> populateLast3Coins() {
+        return this.klaserService.findLatest3();
+    }
 
     @RequestMapping({ "/", "/index" })
     public String index(Model model) {
         return "index";
     }
 
-//    @RequestMapping(value = "/monety", method = RequestMethod.GET)
-//    public String showMainPage(Model model) {
-//        model.addAttribute("MyMessages",  notificationService.getNotificationMessages());
-//        return "klaser";
-//    }
+    @RequestMapping(value = "/monety", method = RequestMethod.GET)
+    public String showMainPage(Model model) {
+        model.addAttribute("MyMessages",  notificationService.getNotificationMessages());
+        return "klaser";
+    }
 
     @RequestMapping("/tosell")
     public String showToSellPage() {
